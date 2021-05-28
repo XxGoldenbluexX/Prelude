29b8d2c1b3829d177be3fd3d6390da0366597a54
Hy3z <axel.vivenot@outlook.fr>
2021-05-28 20:37:19 +0200

    bon c'est dégeulasse et pas ouf mais l'araignée grimpe sur les murs!

--- src/main/java/fr/nekotine/prelude/effigies/Spider.java
+++ src/main/java/fr/nekotine/prelude/effigies/Spider.java
@@ -1,14 +1,22 @@
 package fr.nekotine.prelude.effigies;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import fr.nekotine.prelude.Effigy;
 import fr.nekotine.prelude.PlayerWrapper;
+import fr.nekotine.prelude.PreludeMain;
 
 public class Spider extends Effigy{
-
+	private final int levitationDuration=5;
+	private BukkitTask runnable;
+	
 	public Spider(PlayerWrapper w) {
 		super(w,3000,3000);
 	}
@@ -24,6 +32,32 @@ public class Spider extends Effigy{
 		p.setVelocity(p.getEyeLocation().getDirection().multiply(1));
 	}
 	
+	@EventHandler
+	public void onSneaking(PlayerToggleSneakEvent e) {
+		if(e.getPlayer().equals(getWrapper().getPlayer())) {
+			if(e.isSneaking()) {
+				runnable = new BukkitRunnable() {
+					@Override
+					public void run() {
+						if(isStickingToBlock()) {
+							getWrapper().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, levitationDuration+1, 3, false, false, false));
+						}
+					}
+				}.runTaskTimer(PreludeMain.main, 0, levitationDuration);
+			}else {
+				runnable.cancel();
+			}
+		}
+	}
 	
+	private boolean isStickingToBlock() {
+		Location ploc = getWrapper().getPlayer().getLocation();
+		for(int n=-1;n<=1;n=n+2) {
+			if(ploc.clone().add(n, 0, 0).getBlock().isSolid() || ploc.clone().add(0, 0, n).getBlock().isSolid()) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 }
